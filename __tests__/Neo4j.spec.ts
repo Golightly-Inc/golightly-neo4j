aa867fd4b3b8ce5f60c1d016c1bd5b2147e5bddc
jjbeach <beachjoshua87@gmail.com>
2020-02-04 17:01:14 -0700

    make fetch required param; return array of ids instead of objects

--- __tests__/Neo4j.spec.ts
+++ __tests__/Neo4j.spec.ts
@@ -5,48 +5,91 @@ import { Neo4j } from '../src/index';
 
 describe('Neo4j', () => {
   describe('Initialization', () => {
+    describe('fetchFunc', () => {
+      it('is assigned to function as defined by user', () => { 
+        const expected: Neo4j = new Neo4j(fetchMock);
+
+        expect(expected.fetchFunc).toEqual(fetchMock);
+      })
+    });
     describe('baseUrl instance variable', () => {
       it('is assigned to default url if empty initialization', () => {
-        const expected: Neo4j = new Neo4j();
+        const expected: Neo4j = new Neo4j(fetchMock);
     
         expect(expected.baseUrl).toEqual('https://golightly-neo4j-api.herokuapp.com');
       });
       it('is assigned to url class is initilized with', () => {
-        const expected: Neo4j = new Neo4j('https://example.com');
+        const expected: Neo4j = new Neo4j(fetchMock, 'https://example.com');
   
         expect(expected.baseUrl).toEqual('https://example.com');
       });
     });
+    describe('createNewUserUrl instance variable', () => {
+      it('is assigned to baseUrl/create_user', () => {
+        const expected: Neo4j = new Neo4j(fetchMock);
+    
+        expect(expected.createNewUserUrl).toEqual(`${expected.baseUrl}/create_user`)
+      });
+    });
     describe('establishConnectionUrl instance variable', () => {
       it('is assigned to baseUrl/users/establish_connection', () => {
-        const expected: Neo4j = new Neo4j();
+        const expected: Neo4j = new Neo4j(fetchMock);
     
         expect(expected.establishConnectionUrl).toEqual(`${expected.baseUrl}/users/establish_connection`)
       });
     });
     describe('establishReferralUrl', () => {
       it('is assigned to baseUrl/users/establish_referral', () => {
-        const expected: Neo4j = new Neo4j('fooboo.com');
+        const expected: Neo4j = new Neo4j(fetchMock, 'fooboo.com');
     
         expect(expected.establishReferralUrl).toEqual(`${expected.baseUrl}/users/establish_referral`)
       });
     });
-    describe('fetchFunc', () => {
-      it('is assigned to function as defined by user', () => { 
-        const fakeAFFetch = (input: RequestInfo, init?: RequestInit | undefined) : Promise<Response> => new Promise(() => {})
-        const expected: Neo4j = new Neo4j('',fakeAFFetch);
+  });
 
-        expect(expected.fetchFunc).toEqual(fakeAFFetch);
+  describe('Creating new user', () => {
+    describe('#createNewUser()', () => {
+      let neo4j: Neo4j;
+      let newUserParams: NewUserData;
+      beforeEach(() => {
+        jest.clearAllMocks();
+        neo4j = new Neo4j(fetchMock);
+        newUserParams = {
+          user: {
+            golightly_uuid: '456-boo',
+            referred_by: '123-foo'
+          }
+        }
+      });
+      it('makes one request to baseUrl/create_user', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({}));
+
+        const status = await neo4j.createNewUser(newUserParams);
+        expect(fetchMock.mock.calls.length).toEqual(1);
+        expect(fetchMock.mock.calls[0][0]).toEqual(`${neo4j.baseUrl}/create_user`)
+      });
+      it('returns status 200 on success', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({}));
+
+        const status = await neo4j.createNewUser(newUserParams);
+        expect(status).toEqual(200);
+      });
+      it('returns undefined on failure', async () => {
+        fetchMock.mockReject(new Error('fake error message'));
+  
+        const status = await neo4j.createNewUser(newUserParams);
+        expect(status).toBe(undefined);
+        expect(status).not.toBe(true);
       })
     });
-  });
+  })
 
   describe('Creating connection or referral associations b/w two users', () => {
     let neo4j: Neo4j;
     let usersParams: UserConnectData;
     beforeEach(() => {
       jest.clearAllMocks();
-      neo4j = new Neo4j();
+      neo4j = new Neo4j(fetchMock);
       usersParams = {
         users: {
           user_one: {
@@ -59,35 +102,45 @@ describe('Neo4j', () => {
       }
     });
     describe('#createConnectionEdges()', () => {
-      it('returns status 200 on success', async () => {
+      it('makes one request to /users/establish_connection', async () => {
         fetchMock.mockResponseOnce(JSON.stringify({}));
 
         const status = await neo4j.createConnectionEdges(usersParams);
         expect(fetchMock.mock.calls.length).toEqual(1);
+        expect(fetchMock.mock.calls[0][0]).toEqual(`${neo4j.baseUrl}/users/establish_connection`)
+      });
+      it('returns status 200 on success', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({}));
+
+        const status = await neo4j.createConnectionEdges(usersParams);
         expect(status).toEqual(200);
       });
       it('returns undefined on failure', async () => {
         fetchMock.mockReject(new Error('fake error message'));
   
         const status = await neo4j.createConnectionEdges(usersParams);
-        expect(fetchMock.mock.calls.length).toEqual(1);
         expect(status).toBe(undefined);
         expect(status).not.toBe(true);
       })
     })
     describe('#createReferralEdges()', () => {
+      it('makes one request to baseUrl/users/establish_referral', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({}));
+
+        const status = await neo4j.createReferralEdges(usersParams);
+        expect(fetchMock.mock.calls.length).toEqual(1);
+        expect(fetchMock.mock.calls[0][0]).toEqual(`${neo4j.baseUrl}/users/establish_referral`)
+      });
       it('returns status 200 on success', async () => {
         fetchMock.mockResponseOnce(JSON.stringify({}));
         
         const status = await neo4j.createReferralEdges(usersParams);
-        expect(fetchMock.mock.calls.length).toEqual(1);
         expect(status).toEqual(200);
       });
       it('returns undefined on failure', async () => {
         fetchMock.mockReject(new Error('fake error message'));
   
         const status = await neo4j.createReferralEdges(usersParams);
-        expect(fetchMock.mock.calls.length).toEqual(1);
         expect(status).toBe(undefined);
         expect(status).not.toBe(true);
       })
@@ -98,7 +151,7 @@ describe('Neo4j', () => {
     let neo4j: Neo4j;
     beforeEach(() => {
       jest.clearAllMocks();
-      neo4j = new Neo4j();
+      neo4j = new Neo4j(fetchMock);
     });
     describe('#retrieveMutualConnections()', () => {
       it('returns a message object if params are equal', async () => {
@@ -106,13 +159,20 @@ describe('Neo4j', () => {
 
         expect(result).toEqual({ message: 'No mutual connections exist for these users' })
       });
-      it('returns an array of golightly_uuid objets on success when mutual connections exist', async () => {
+      it('makes one request to baseUrl/mutual_connections?golightly_uuid_one=${uuidOne}&golightly_uuid_two=${uuidTwo}', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({}));
+
+        const status = await neo4j.retrieveMutualConnections('string1', 'string2');
+        expect(fetchMock.mock.calls.length).toEqual(1);
+        expect(fetchMock.mock.calls[0][0]).toEqual(`${neo4j.baseUrl}/mutual_connections?golightly_uuid_one=string1&golightly_uuid_two=string2`)
+      });
+      it('returns an array of golightly_uuids on success when mutual connections exist', async () => {
         fetchMock.mockResponseOnce(JSON.stringify([{ golightly_uuid: '789-cool' }, { golightly_uuid: '001-xyz' }]));
         const uuidOne: string = '123-foo';
         const uuidTwo: string = '456-boo';
 
         const result = await neo4j.retrieveMutualConnections(uuidOne, uuidTwo);
-        expect(result).toEqual([{golightly_uuid: '789-cool'}, {golightly_uuid: '001-xyz'}]);
+        expect(result).toEqual(['789-cool', '001-xyz']);
       });
       it('returns a message object on success when no mutual connections exist', async () => {
         fetchMock.mockResponseOnce(JSON.stringify({ message: 'No mutual connections exist for these users' }));
@@ -138,14 +198,21 @@ describe('Neo4j', () => {
       let neo4j: Neo4j;
       beforeEach(() => {
         jest.clearAllMocks();
-        neo4j = new Neo4j();
+        neo4j = new Neo4j(fetchMock);
       });
       it('returns a message object if params are equal', async () => {
         const result = await neo4j.retrieveShortestPathConnections('uuidOne', 'uuidOne');
 
         expect(result).toEqual({ message: 'No connections exist for these users' })
       });
-      it('returns an array of golightly_uuid objets on success when connections exist b/w', async () => {
+      it('makes one request to baseUrl/shortest_path?golightly_uuid_one=${uuidOne}&golightly_uuid_two=${uuidTwo}', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({}));
+
+        const status = await neo4j.retrieveShortestPathConnections('string1', 'string2');
+        expect(fetchMock.mock.calls.length).toEqual(1);
+        expect(fetchMock.mock.calls[0][0]).toEqual(`${neo4j.baseUrl}/shortest_path?golightly_uuid_one=string1&golightly_uuid_two=string2`)
+      });
+      it('returns an array of golightly_uuids on success when connections exist b/w', async () => {
         const nodes = [
           { golightly_uuid: '123-foo' }, 
           { golightly_uuid: '000-ooo' }, 
@@ -157,7 +224,7 @@ describe('Neo4j', () => {
         const uuidTwo: string = '456-boo';
 
         const result = await neo4j.retrieveShortestPathConnections(uuidOne, uuidTwo);
-        expect(result).toEqual(nodes);
+        expect(result).toEqual(['000-ooo', '001-coo']);
       });
       it('returns a message object on success when no mutual connections exist', async () => {
         fetchMock.mockResponseOnce(JSON.stringify({ message: 'No connections exist for these users' }));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 declare global {
+
+  interface NewUserData {
+    user: {
+      golightly_uuid: string
+      referred_by: string
+    }
+  }
+
   interface UserConnectData {
     users: {
       user_one: {
@@ -20,24 +28,30 @@ declare global {
 }
 
 export class Neo4j {
+  fetchFunc: ((input: RequestInfo, init?: RequestInit | undefined) => Promise<Response>)
   baseUrl: string;
   establishConnectionUrl: string;
   establishReferralUrl: string;
-  fetchFunc: ((input: RequestInfo, init?: RequestInit | undefined) => Promise<Response>)
+  createNewUserUrl: string;
 
-  constructor(baseUrl?: string, fetchFunc?: ((input: RequestInfo, init?: RequestInit | undefined) => Promise<Response>)) {
+  constructor(fetchFunc: ((input: RequestInfo, init?: RequestInit | undefined) => Promise<Response>), baseUrl?: string) {
+    this.fetchFunc = fetchFunc;
     this.baseUrl = baseUrl || 'https://golightly-neo4j-api.herokuapp.com';
+    this.createNewUserUrl = `${this.baseUrl}/create_user`;
     this.establishConnectionUrl = `${this.baseUrl}/users/establish_connection`;
     this.establishReferralUrl = `${this.baseUrl}/users/establish_referral`;
-    this.fetchFunc = fetchFunc || fetch
-  }  
+  }
+
+  async createNewUser(newUserParams: NewUserData) {
+    return await postToNeo4jServer(this.createNewUserUrl, newUserParams, this.fetchFunc);
+  }
 
   async createConnectionEdges(usersParams: UserConnectData) {
-    return await createConnectionOrReferral(this.establishConnectionUrl, usersParams, this.fetchFunc)
+    return await postToNeo4jServer(this.establishConnectionUrl, usersParams, this.fetchFunc);
   }
 
   async createReferralEdges(usersParams: UserConnectData) {
-    return await createConnectionOrReferral(this.establishReferralUrl, usersParams, this.fetchFunc)
+    return await postToNeo4jServer(this.establishReferralUrl, usersParams, this.fetchFunc);
   }
 
   async retrieveMutualConnections(uuidOne: string, uuidTwo: string) {
@@ -45,7 +59,11 @@ export class Neo4j {
       return { message: 'No mutual connections exist for these users' };
     }
     const URL = `${this.baseUrl}/mutual_connections?golightly_uuid_one=${uuidOne}&golightly_uuid_two=${uuidTwo}`;
-    return await getNeo4jNodes(URL, this.fetchFunc);
+    const resp = await getNeo4jNodes(URL, this.fetchFunc);
+    if ((resp as Neo4jResponseObjType[]) && (resp as Neo4jResponseObjType[]).length >0) {
+      return (resp as Neo4jResponseObjType[]).map(mc => mc.golightly_uuid);
+    }
+    return resp
   }
 
   async retrieveShortestPathConnections(uuidOne: string, uuidTwo: string) {
@@ -53,11 +71,15 @@ export class Neo4j {
       return { message: 'No connections exist for these users' };
     }
     const URL = `${this.baseUrl}/shortest_path?golightly_uuid_one=${uuidOne}&golightly_uuid_two=${uuidTwo}`;
-    return await getNeo4jNodes(URL, this.fetchFunc);
+    const resp = await getNeo4jNodes(URL, this.fetchFunc);
+    if ((resp as Neo4jResponseObjType[]) && (resp as Neo4jResponseObjType[]).length >0) {
+      return (resp as Neo4jResponseObjType[]).map(c => c.golightly_uuid).filter(id => id !== uuidOne && id !== uuidTwo);
+    }
+    return resp
   }
 }
 
-async function createConnectionOrReferral(url: string, usersParams: UserConnectData, fetchFunc: ((input: RequestInfo, init?: RequestInit | undefined) => Promise<Response>)) {
+async function postToNeo4jServer(url: string, params: NewUserData | UserConnectData, fetchFunc: ((input: RequestInfo, init?: RequestInit | undefined) => Promise<Response>)) {
   let result: number | undefined;
   try {
     const resp = await fetchFunc(url, {
@@ -66,7 +88,7 @@ async function createConnectionOrReferral(url: string, usersParams: UserConnectD
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(usersParams),
+      body: JSON.stringify(params),
     });
     const { status } = resp;
     result  = status;
